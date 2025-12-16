65fc9b5afa4018d22209e7a7644546a498fa0460
Kenny Varghese <kennyvarghese@Kennys-MacBook-Air.local>
2025-12-15 20:53:11 -0800

    grouped tasks by assignee and added done completion next to it

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import { useEffect, useState } from "react";
 import { getData } from "./data";
 import type { ITask } from "./data";
 
+type GroupedTasks = Record<string, ITask[]>;
+
+function groupTasksByAssigned(tasks: ITask[]): GroupedTasks{
+  const grouped: GroupedTasks = {};
+
+  for(const task of tasks) {
+    const assigneeName = task.assignee ? task.assignee.firstName : "Not Assigned";
+    if(!grouped[assigneeName]) grouped[assigneeName] = [];
+
+    grouped[assigneeName].push(task);
+  }
+
+  return grouped;
+}
+
 function App() {
   const [tasks, setTasks] = useState<ITask[]>([]);
   const [loading, setLoading] = useState(true);
@@ -20,11 +35,28 @@ function App() {
     return <div>Getting tasks...</div>;
   }
 
+  const groupedTasks = groupTasksByAssigned(tasks);
 
   return (
     <div>
       <h1>Task Board</h1>
       <p>total tasks: {tasks.length}</p>
+      
+      <div className="columns">
+          {Object.entries(groupedTasks).map(([assignee, taskList]) => (
+            <div key={assignee}>
+              <h2>{assignee}</h2>
+              <ul className="task-list">
+                {taskList.map((task) => (
+                  <li key={task.id}>
+                    {task.title} {task.isCompleted ? " | done" : ""}
+                  </li>
+                ))}
+              </ul>
+            </div>
+        ))}
+      </div>
+
     </div>
 
   );
